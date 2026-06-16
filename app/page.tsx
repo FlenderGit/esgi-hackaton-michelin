1cde63d53706ed107aa47e22960736abe59b63e0
FlenderGit <tristan.deloeil@gmail.com>
2026-06-16 11:44:06 +0200

    FIX: remove unused extensin

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import SuppliersList from "./testsupplier";
 
 export default function Home() {
   return (
@@ -51,7 +50,6 @@ export default function Home() {
             />
             Deploy Now
           </a>
-          <SuppliersList />
           <a
             className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
